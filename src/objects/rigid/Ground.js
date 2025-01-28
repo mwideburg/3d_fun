01e6bf0ba46644327b3dc335a1b1ba774faed3c2
mjwideburg <michael.wideburg@efwnow.com>
2025-01-28 11:18:07 -0500

    WIP

--- src/objects/rigid/Ground.js
+++ src/objects/rigid/Ground.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 import * as CANNON from 'cannon-es'
 export class Ground {
-    constructor(texturePath, opacity = 1, transparent = false, color = 0x0fffff) {
+    constructor(color = 0x0fffff) {
 
         // Load a low-resolution pixelated texture
         var loader = new THREE.TextureLoader();
-        var groundTexture = loader.load(`${texturePath}`);
+        var groundTexture = loader.load(`grass.png`);
         groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
         groundTexture.repeat.set(25, 25);
-        // groundTexture.anisotropy = 16;
+
         groundTexture.encoding = THREE.sRGBEncoding;
 
         const geometry = new THREE.BoxGeometry(500, 500, 1);
@@ -16,15 +16,13 @@ export class Ground {
             color,
             side: THREE.DoubleSide,
             map: groundTexture,
-            opacity,
-            transparent
         });
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.receiveShadow = true;
 
         this.physMat = new CANNON.Material();
         this.body = new CANNON.Body({
-            shape: new CANNON.Box(new CANNON.Vec3(20, 450, 0.5)),
+            shape: new CANNON.Box(new CANNON.Vec3(250, 250, 0.5)),
             type: CANNON.Body.STATIC,
             material: this.physMat
         });
